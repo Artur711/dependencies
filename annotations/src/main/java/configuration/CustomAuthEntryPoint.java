03c7a8a897384dc3191b8a4f76ba2cd3623ef4f9
Artur Kubiak <arturkubiak9@gmail.com>
2022-01-03 12:39:00 +0100

    First commit

---
+++ annotations/src/main/java/configuration/CustomAuthEntryPoint.java
@@ -0,0 +1,35 @@
+package configuration;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static java.lang.String.format;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
+@Component
+public class CustomAuthEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName() + "");
+        response.setStatus(SC_UNAUTHORIZED);
+        final PrintWriter writer = response.getWriter();
+        writer.println(format("HTTP Status 401 - %s", authException.getMessage()));
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("JDeveloper");
+        super.afterPropertiesSet();
+    }
+}
