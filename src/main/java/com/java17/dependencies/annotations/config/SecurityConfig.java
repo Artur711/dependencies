0b13e3b8feb174151eb0336247f268cefa097873
artur <arturkubiak9@gmail.com>
2022-10-10 21:05:33 +0200

    Update to spring boot 2.74.

--- src/main/java/com/java17/dependencies/annotations/config/SecurityConfig.java
+++ src/main/java/com/java17/dependencies/annotations/config/SecurityConfig.java
@@ -1,20 +1,21 @@
 package com.java17.dependencies.annotations.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final CustomAuthEntryPoint customAuthEntryPoint;
 
@@ -35,23 +36,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.customAuthEntryPoint = customAuthEntryPoint;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and()
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .authenticationEntryPoint(customAuthEntryPoint);
+        return http.build();
     }
 
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication()
-                        .withUser(rootUser).password(encoder.encode(rootPassword)).roles("ADMIN")
-                .and()
-                        .withUser(serviceUser).password(encoder.encode(servicePassword)).roles("USER");
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername(serviceUser)
+                .password(bCryptPasswordEncoder.encode(servicePassword))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername(rootUser)
+                .password(bCryptPasswordEncoder.encode(rootPassword))
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
     }
 }
