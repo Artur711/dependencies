ce78d668a79b86dbbbfa4987afe6a2ec0134a9ea
artur <arturkubiak9@gmail.com>
2023-03-04 11:34:26 +0100

    Change way of default parameters

--- src/main/java/com/java17/dependencies/annotations/config/SecurityConfig.java
+++ src/main/java/com/java17/dependencies/annotations/config/SecurityConfig.java
@@ -21,20 +21,25 @@ import static com.java17.dependencies.annotations.util.ROLE.ROLE_USER;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     private final CustomAuthEntryPoint customAuthEntryPoint;
+    private final static String ROOT_USER = "root-user";
+    private final static String ROOT_PASSWORD = "root-password";
+    private final static String SERVICE_USER = "user";
+    private final static String SERVICE_PASSWORD = "password";
+    private final static String ACTUATOR_PATH = "/app";
 
-    @Value("${auth.root.user:root-user}")
+    @Value("${auth.root.user}")
     private String rootUser;
 
-    @Value("${auth.root.password:root-password}")
+    @Value("${auth.root.password}")
     private String rootPassword;
 
-    @Value("${auth.service.user:user}")
+    @Value("${auth.service.user}")
     private String serviceUser;
 
-    @Value("${auth.service.password:password}")
+    @Value("${auth.service.password}")
     private String servicePassword;
 
-    @Value("${management.endpoints.web.base-path:/app}")
+    @Value("${management.endpoints.web.base-path}")
     private String actuatorPath;
 
 
@@ -49,11 +54,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        String healthPath = actuatorPath.isBlank() ? ACTUATOR_PATH : actuatorPath;
         http.csrf().disable()
                 .cors().and()
                 .authorizeRequests()
 //                .antMatchers(actuatorPath + "/**", "/swagger-ui/**", "/v3/**")
-                .antMatchers(actuatorPath + "/**")
+                .antMatchers(healthPath + "/**")
                 .hasAnyRole(ROLE_ADMIN.roleName())
                 .anyRequest().authenticated()
                 .and()
@@ -65,12 +71,12 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername(serviceUser)
-                .password(passwordEncoder.encode(servicePassword))
+        manager.createUser(User.withUsername(serviceUser.isBlank() ? SERVICE_USER : serviceUser)
+                .password(passwordEncoder.encode(servicePassword.isBlank() ? SERVICE_PASSWORD : servicePassword))
                 .roles(ROLE_USER.roleName())
                 .build());
-        manager.createUser(User.withUsername(rootUser)
-                .password(passwordEncoder.encode(rootPassword))
+        manager.createUser(User.withUsername(rootUser.isBlank() ? ROOT_USER : rootUser)
+                .password(passwordEncoder.encode(rootPassword.isBlank() ? ROOT_PASSWORD : rootPassword))
                 .roles(ROLE_USER.roleName(), ROLE_ADMIN.roleName())
                 .build());
         return manager;
